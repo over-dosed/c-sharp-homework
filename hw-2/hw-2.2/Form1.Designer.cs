89c3d368276c16276dca4623e70966f411a136d7
2017247946 <2017247946@qq.com>
2021-03-03 18:10:19 +0800

    just finish hw2-2

--- hw-2/hw-2.2/Form1.Designer.cs
+++ hw-2/hw-2.2/Form1.Designer.cs
@@ -29,13 +29,149 @@ namespace hw_2._2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.arrayInputText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maxNumberLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.minNumberLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.arrNumberLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.sumNumberLabel = new System.Windows.Forms.Label();
+            this.calculateArrayButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // arrayInputText
+            // 
+            this.arrayInputText.Location = new System.Drawing.Point(339, 111);
+            this.arrayInputText.Name = "arrayInputText";
+            this.arrayInputText.Size = new System.Drawing.Size(335, 25);
+            this.arrayInputText.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "please input an array:\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "max number of the array:";
+            // 
+            // maxNumberLabel
+            // 
+            this.maxNumberLabel.AutoSize = true;
+            this.maxNumberLabel.Location = new System.Drawing.Point(339, 156);
+            this.maxNumberLabel.Name = "maxNumberLabel";
+            this.maxNumberLabel.Size = new System.Drawing.Size(0, 15);
+            this.maxNumberLabel.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(116, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "min number of the array:\r\n";
+            // 
+            // minNumberLabel
+            // 
+            this.minNumberLabel.AutoSize = true;
+            this.minNumberLabel.Location = new System.Drawing.Point(339, 194);
+            this.minNumberLabel.Name = "minNumberLabel";
+            this.minNumberLabel.Size = new System.Drawing.Size(0, 15);
+            this.minNumberLabel.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(116, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(199, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "arr number of the array:\r\n";
+            // 
+            // arrNumberLabel
+            // 
+            this.arrNumberLabel.AutoSize = true;
+            this.arrNumberLabel.Location = new System.Drawing.Point(339, 233);
+            this.arrNumberLabel.Name = "arrNumberLabel";
+            this.arrNumberLabel.Size = new System.Drawing.Size(0, 15);
+            this.arrNumberLabel.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(116, 269);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(199, 15);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "sum number of the array:\r\n";
+            // 
+            // sumNumberLabel
+            // 
+            this.sumNumberLabel.AutoSize = true;
+            this.sumNumberLabel.Location = new System.Drawing.Point(339, 269);
+            this.sumNumberLabel.Name = "sumNumberLabel";
+            this.sumNumberLabel.Size = new System.Drawing.Size(0, 15);
+            this.sumNumberLabel.TabIndex = 5;
+            // 
+            // calculateArrayButton
+            // 
+            this.calculateArrayButton.Location = new System.Drawing.Point(680, 111);
+            this.calculateArrayButton.Name = "calculateArrayButton";
+            this.calculateArrayButton.Size = new System.Drawing.Size(108, 26);
+            this.calculateArrayButton.TabIndex = 6;
+            this.calculateArrayButton.Text = "calculate";
+            this.calculateArrayButton.UseVisualStyleBackColor = true;
+            this.calculateArrayButton.Click += new System.EventHandler(this.calculateArrayButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.calculateArrayButton);
+            this.Controls.Add(this.sumNumberLabel);
+            this.Controls.Add(this.arrNumberLabel);
+            this.Controls.Add(this.minNumberLabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.maxNumberLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.arrayInputText);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox arrayInputText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label maxNumberLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label minNumberLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label arrNumberLabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label sumNumberLabel;
+        private System.Windows.Forms.Button calculateArrayButton;
     }
 }
 
--- hw-2/hw-2.2/Form1.cs
+++ hw-2/hw-2.2/Form1.cs
@@ -15,6 +15,32 @@ namespace hw_2._2
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void calculateArrayButton_Click(object sender, EventArgs e)
+        {
+            const int arrayNum = 20;//定义数组最大数为20
+            int[] num = new int[arrayNum];
+            string[] sArray = arrayInputText.Text.Split(new char[3] { ' ', ',','，' }); //将用户输入的字符串以空格和逗号区分
+            for (int i = 0; i < sArray.Length; i++)
+            {
+                num[i] = int.Parse(sArray[i]);
+            }
+            int max = num[0], min = num[0], arr = 0, sum = 0;
+            int arrayNumCount = 0;//用于计数-数组中多少个元素
+            for(arrayNumCount = 0; arrayNumCount < arrayNum && num[arrayNumCount] != 0; arrayNumCount++)
+            {
+                max = num[arrayNumCount] > max ? num[arrayNumCount] : max;
+                min = num[arrayNumCount] < min ? num[arrayNumCount] : min;
+                sum += num[arrayNumCount];
+            }
+            arr = sum / arrayNumCount;
+
+            maxNumberLabel.Text = max.ToString();
+            minNumberLabel.Text = min.ToString();
+            arrNumberLabel.Text = arr.ToString();
+            sumNumberLabel.Text = sum.ToString();
         }
     }
 }
