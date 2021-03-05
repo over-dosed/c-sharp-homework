dcf36be6497d8c9ef501c95a256a108f4ff4a7c1
2017247946 <2017247946@qq.com>
2021-03-05 12:57:04 +0800

    first write hw-2.3

---
+++ hw-2/hw-2.3,2.4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hw_2._3_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int minNumber = 2;
+            const int maxNumber = 100;
+
+        }
+    }
+}
