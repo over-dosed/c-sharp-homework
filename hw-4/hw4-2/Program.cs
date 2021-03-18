c6cdbbd2457845eb32c2929a0ca4e847d5ba46ca
2017247946 <2017247946@qq.com>
2021-03-18 23:51:37 +0800

    just start the hw4-2

---
+++ hw-4/hw4-2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace hw4_2
+{
+    class Time
+    {
+        public int hour { set; get; }
+        public int min { set; get; }
+        public int second { set; get; }
+
+        public Time(int h, int m, int s)
+        {
+            hour = h; min = m; second = s;
+        }
+    }
+
+    public delegate 
+    class Clock
+    {
+        public Time clockTime { set; get; }
+
+        public Time alarmTime { set; get; }
+
+        public void setClockTime(int h, int m, int s)
+        {
+            clockTime.hour = h; clockTime.min = m; clockTime.second = s;
+        }
+
+        public void setAlarmTime(int h, int m, int s)
+        {
+            alarmTime.hour = h; alarmTime.min = m; alarmTime.second = s;
+        }
+        public void show
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
